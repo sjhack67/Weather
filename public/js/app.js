e28912a12cf465537a692c5762f0a15b609f1dd4
Shreyash Jangam <69251229+sjhack67@users.noreply.github.com>
2022-03-30 22:28:21 +0530

    aadded files for website

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weather=document.querySelector('form')
+const search=document.querySelector('input')
+const messageOne=document.querySelector('#msg-1')
+const messageTwo=document.querySelector('#msg-2')
+
+
+
+weather.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    messageOne.textContent="Loading..."
+    messageTwo.textContent=""
+
+    const location=search.value;
+    fetch("/weather?address="+location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                return messageOne.textContent=data.error;
+            }
+            messageOne.textContent=data.location;
+            messageTwo.textContent=data.forecast;
+        })
+    })
+})
